e2ce9fa919df8e7613f9fd8a94be0024a7e081ec
SirRight12 <119626466+SirRight12@users.noreply.github.com>
2026-06-07 12:00:37 -0500

    add all the group activities

--- Scripts/groups.js
+++ Scripts/groups.js
@@ -1,18 +1,18 @@
 let group = 'master'
 const groups = {
-    'master': ['Large Group Opening', 'First Block', 'Second Block', 'Third Block', 'Fourth Block', 'Large Group Closing'],
-    'lime': [],
-    'blue': [],
-    'orange': [],
-    'purple': [],
-    'green': [],
-    'red': [],
-    'gray': [],
-    'yellow': [],
-    'navy': [],
-    'teal': [],
-    'black': [],
-    'pink': [],
+    'master': ['Sanctuary','Large Group Opening', 'First Block', 'Second Block', 'Third Block', 'Fourth Block', 'Large Group Closing','Sanctuary'],
+    'lime': ['Sanctuary','Large Group Opening','Snack/Missions','Games','Crafts','Small Group','Large Group Closing','Sanctuary'],
+    'blue': ['Sanctuary','Large Group Opening','Snack/Missions','Games','Crafts','Small Group','Large Group Closing','Sanctuary'],
+    'orange': ['Sanctuary','Large Group Opening','Snack/Missions','Games','Small Group','Crafts','Large Group Closing','Sanctuary'],
+    'purple': ['Sanctuary','Large Group Opening','Snack/Missions','Games','Small Group','Crafts','Large Group Closing','Sanctuary'],
+    'green': ['Sanctuary','Large Group Opening','Games','Crafts','Snack/Missions','Small Group','Large Group Closing','Sanctuary'],
+    'red': ['Sanctuary','Large Group Opening','Games','Snack/Missions','Crafts','Small Group','Large Group Closing','Sanctuary'],
+    'gray': ['Sanctuary','Large Group Opening','Crafts','Small Group','Snack/Missions','Games','Large Group Closing','Sanctuary'],
+    'yellow': ['Sanctuary','Large Group Opening','Crafts','Snack/Missions','Small Group','Games','Large Group Closing','Sanctuary'],
+    'navy': ['Sanctuary','Large Group Opening','Games','Crafts','Small Group','Snack/Missions','Large Group Closing','Sanctuary'],
+    'teal': ['Sanctuary','Large Group Opening','Games','Crafts','Small Group','Snack/Missions','Large Group Closing','Sanctuary'],
+    'black': ['Sanctuary','Large Group Opening','Small Group','Snack/Missions','Games','Crafts','Large Group Closing','Sanctuary'],
+    'pink': ['Sanctuary','Large Group Opening','Small Group','Crafts','Games','Snack/Missions','Large Group Closing','Sanctuary'],
 }
 function changeGroup(groupName) {
     group = groupName;
